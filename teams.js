66e8d0f9f21571d8e6c8e946d4bfadd7cfa88d64
Austin Heiman <atheimanksu@gmail.com>
2015-03-09 20:50:56 -0500

    initial commit to GH

---
+++ teams.js
@@ -0,0 +1,13 @@
+// Big 12 teams in order of conference standing
+var teams = [
+    "Kansas",
+    "Oklahoma",
+    "Texas",
+    "Iowa State",
+    "K-State",
+    "W. Virginia",
+    "Baylor",
+    "Okla. State",
+    "Texas Tech",
+    "TCU"
+];
